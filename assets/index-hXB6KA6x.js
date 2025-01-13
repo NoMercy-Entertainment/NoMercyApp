73afce72b6537e3f8d6515f1748e85facd30b4e6
StoneyEagle <StoneyEagle@users.noreply.github.com>
2025-01-13 16:51:24 +0000

    deploy: 97b1f48b9abdfc282bc16995ba78fd77fd60aa98

---
+++ assets/index-hXB6KA6x.js
@@ -0,0 +1,3 @@
+const __vite__fileDeps=["assets/web-CU5HdxR6.js","assets/index-r5nHMicN.js","assets/index-BsxCsYkp.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{bp as n,bq as r}from"./index-r5nHMicN.js";const i=n("ScreenOrientation",{web:()=>r(()=>import("./web-CU5HdxR6.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ScreenOrientationWeb)});export{i as ScreenOrientation};
+//# sourceMappingURL=index-hXB6KA6x.js.map
