d03e4539611142aa4cd8c471ca3f7f52b48c59f2
StoneyEagle <StoneyEagle@users.noreply.github.com>
2025-01-13 22:54:28 +0000

    deploy: a2e1d32ebff16f91791703ac3bc890a78fc9bc6e

---
+++ assets/Mobile-wpm9Mjxa.js
@@ -0,0 +1,2 @@
+import{aw as t,e as o,a7 as a,aB as e,o as r,v as n,bY as s,bd as l,bE as u,bF as c}from"./index-16sqk8Bx.js";const i=t({__name:"Mobile",setup(_){return(p,f)=>(r(),o(e(c),null,{default:a(()=>[n(e(u),{fullscreen:!0},{default:a(()=>[n(s,{name:`${e(l).currentRoute.value.name} Mobile page`},null,8,["name"])]),_:1})]),_:1}))}});export{i as default};
+//# sourceMappingURL=Mobile-wpm9Mjxa.js.map
