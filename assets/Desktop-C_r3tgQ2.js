8f07ab7d7416c40320e44875009c209b89d483f8
StoneyEagle <StoneyEagle@users.noreply.github.com>
2025-01-17 01:49:18 +0000

    deploy: c1fa58d2a94025fb6712b9dec2637e17ed3498ba

--- assets/Desktop-C_r3tgQ2.js
+++
@@ -1,2 +0,0 @@
-import{aw as o,az as n,aD as s,e as r,a7 as e,aB as a,o as l,v as t,bZ as u,bF as p,bG as c}from"./index-B3YEJJIP.js";const m=o({__name:"Desktop",setup(_){return n(()=>{s(null)}),(f,i)=>(l(),r(a(c),null,{default:e(()=>[t(a(p),{fullscreen:!0},{default:e(()=>[t(u,{name:"Libraries Desktop page"})]),_:1})]),_:1}))}});export{m as default};
-//# sourceMappingURL=Desktop-C_r3tgQ2.js.map
---
+++ assets/index-yHuZRGfj.js
@@ -0,0 +1,3 @@
+const __vite__fileDeps=["assets/web-BsBtRHic.js","assets/index-nP_NhIFx.js","assets/index-CGmtu3_z.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{bq as n,br as r}from"./index-nP_NhIFx.js";const i=n("ScreenOrientation",{web:()=>r(()=>import("./web-BsBtRHic.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ScreenOrientationWeb)});export{i as ScreenOrientation};
+//# sourceMappingURL=index-yHuZRGfj.js.map
