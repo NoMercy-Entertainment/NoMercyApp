81e471e9fdf0e2b83c925140727c9e373afa8f3e
StoneyEagle <StoneyEagle@users.noreply.github.com>
2025-01-23 01:03:15 +0000

    deploy: 0eaa18f0d2a275776406a9225412c99593428ffa

---
+++ assets/Tv-BrABFTBb.js
@@ -0,0 +1,2 @@
+import{aw as n,e as o,a7 as e,aB as a,o as s,v as t,bZ as r,bF as c,bG as l}from"./index-ByXhQ52S.js";const m=n({__name:"Tv",setup(u){return(_,p)=>(s(),o(a(l),null,{default:e(()=>[t(a(c),{fullscreen:!0},{default:e(()=>[t(r,{name:"Dashboard System Tv page"})]),_:1})]),_:1}))}});export{m as default};
+//# sourceMappingURL=Tv-BrABFTBb.js.map
