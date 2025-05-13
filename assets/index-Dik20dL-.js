72709f698507c4439218b607a69a617f1952f299
StoneyEagle <StoneyEagle@users.noreply.github.com>
2025-05-13 18:00:12 +0000

    deploy: 12e4c26730d0e7358f5dff995dfac79106473edf

--- assets/index-Dik20dL-.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D8fDb5G8.js","assets/index-HwwydxHE.js","assets/index-CXUmIRrJ.css"])))=>i.map(i=>d[i]);
-import{bA as r,bB as t}from"./index-HwwydxHE.js";const i=r("ScreenOrientation",{web:()=>t(()=>import("./web-D8fDb5G8.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ScreenOrientationWeb)});export{i as ScreenOrientation};
