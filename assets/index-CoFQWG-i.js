06608c78453393b05f284543893e2cd592b1a00c
StoneyEagle <StoneyEagle@users.noreply.github.com>
2025-05-28 22:28:57 +0000

    deploy: 30344ae4c48a376e4d4dec84124e4b6c1a099c38

--- assets/index-CoFQWG-i.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bx0U52k7.js","assets/index-gzUCJUAv.js","assets/index-BcdhN7vg.css"])))=>i.map(i=>d[i]);
-import{bA as r,bB as t}from"./index-gzUCJUAv.js";const i=r("ScreenOrientation",{web:()=>t(()=>import("./web-Bx0U52k7.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ScreenOrientationWeb)});export{i as ScreenOrientation};
--- assets/web-CwHUF9mZ.js
+++
@@ -1,1 +0,0 @@
-import{bz as o}from"./index-gzUCJUAv.js";class s extends o{async isWatching(){throw new Error("VolumeButtons is not supported on web")}async watchVolume(e,n){throw new Error("VolumeButtons is not supported on web")}async clearWatch(){throw new Error("VolumeButtons is not supported on web")}}export{s as VolumeButtonsWeb};
