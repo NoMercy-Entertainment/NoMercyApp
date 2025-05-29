2e5f9e9151ee9ef7555947229050b43ad0d654e7
StoneyEagle <StoneyEagle@users.noreply.github.com>
2025-05-29 00:48:06 +0000

    deploy: bdc9d2e8608f4384c2b5c470786f02ef142a0c60

---
+++ assets/web-kvROdOJa.js
@@ -0,0 +1,1 @@
+import{bz as o}from"./index-BKfQxFHq.js";class s extends o{async isWatching(){throw new Error("VolumeButtons is not supported on web")}async watchVolume(e,n){throw new Error("VolumeButtons is not supported on web")}async clearWatch(){throw new Error("VolumeButtons is not supported on web")}}export{s as VolumeButtonsWeb};
