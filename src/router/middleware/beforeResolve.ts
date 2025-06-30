c79e90ecf4f6533f644090ea6f988c5a66d79558
Stoney_Eagle <45034970+StoneyEagle@users.noreply.github.com>
2025-07-01 01:18:46 +0200

    fix: correct class styles in InfoHeaderItem.vue for consistent appearance

--- src/router/middleware/beforeResolve.ts
+++ src/router/middleware/beforeResolve.ts
@@ -1,7 +1,7 @@
 import getLibraries from '@/lib/middleware/getLibraries';
 import getLocations from '@/lib/middleware/getLocations';
 import initializeAudioPlayer from '@/lib/middleware/inititalizeAudioPlayer';
-import initializeVideoSocket from '@/lib/middleware/initializeVideoSocket';
+// import initializeVideoSocket from '@/lib/middleware/initializeVideoSocket';
 import initializeMusicSocket from '@/lib/middleware/initializeMusicSocket';
 import getServerPermissions from '@/lib/middleware/getServerPermissions';
 import { storeUserDetails } from '@/lib/middleware/storeUserDetails';
@@ -11,7 +11,7 @@ const promises: Array<() => Promise<void>> = [
 	getLocations,
 	getServerPermissions,
 	getLibraries,
-	initializeVideoSocket,
+	// initializeVideoSocket,
 	initializeMusicSocket,
 	initializeAudioPlayer,
 ];
--- src/views/Base/Info/components/InfoHeaderItem.vue
+++ src/views/Base/Info/components/InfoHeaderItem.vue
@@ -3,7 +3,7 @@
 
 <template>
 	<div
-		class="flex justify-center items-center flex-grow-0 flex-shrink-0 h-7 px-2 py-1 rounded-md border bg-[#05294d]/[0.03] border-[#021c37]/[0.08] dark:bg-[#e2f0fd]/[0.8] dark:border-[#dfeffe]/[0.14]"
+		class="flex justify-center items-center flex-grow-0 flex-shrink-0 h-7 px-2 py-1 rounded-md bg-[#05294d]/[0.03] border-[#021c37]/[0.08] dark:bg-[#e2f0fd]/[0.08] border dark:border-[#dfeffe]/[0.14]"
 	>
 		<div
 			class="relative flex flex-shrink-0 flex-grow-0 items-center justify-center gap-2 px-1"
